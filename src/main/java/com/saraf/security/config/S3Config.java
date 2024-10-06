8e2c13878bd7a10d2d36342355fbb165264ac41b
Amine <uhammudz@gmail.com>
2024-10-05 21:14:17 -0700

    S3 receipt bug fix 06 (from profile to env)

--- src/main/java/com/saraf/security/config/S3Config.java
+++ src/main/java/com/saraf/security/config/S3Config.java
@@ -1,6 +1,7 @@
 package com.saraf.security.config;
 
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -13,7 +14,7 @@ public class S3Config {
     @Bean
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
-                .withCredentials(new ProfileCredentialsProvider("mohamed"))
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
                 .withRegion("us-west-1")
                 .build();
     }
